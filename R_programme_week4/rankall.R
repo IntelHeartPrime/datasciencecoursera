46f7c7ca51b5de71b5a9b460f3f37bfd35c17471
intelheart <yanxin_951226@163.com>
2019-02-20 10:26:00 +0800

    update files

--- R_programme_week4/rankall.R
+++ R_programme_week4/rankall.R
@@ -3,9 +3,23 @@ rankall <- function(outcome, num = "best") { ## Read outcome data
   ## For each state, find the hospital of the given rank
   ## Return a data frame with the hospital names and the ## (abbreviated) state name
   
-  ##get the enum of State Vector
+  ##get the enum of State Vector of State
   ##use the lapply 
   ##the function  rankhospital.function
   ##get the index in the origin DataFrame
-  
+  #library
+  source('~/datasciencecoursera/R_programme_week4/rankhospital.R')
+  outcome1<-read.csv("outcome-of-care-measures.csv")
+  #get the state
+  enum_index<-duplicated(as.vector(outcome1[,7]))
+  enum<-as.array(outcome1[,7])[!enum_index]
+  enum<-as.matrix(enum)
+  #use the lapply of the function of rankhospital.R
+  result<-apply(enum,1,rankhospital,outcome,num)
+  ##print(result)
+  output<-cbind(as.vector(enum),as.character(result))
+  ##print(output)
+  ##also
+  ## result<-apply(enum,2,rankhospital,outcome,num)
 }
+
